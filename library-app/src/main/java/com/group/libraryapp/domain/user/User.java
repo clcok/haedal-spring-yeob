cf9a01759e8ba8aaa4261b85818c86db0f4dce94
clcok <ekfrha24@naver.com>
2024-05-09 19:06:17 +0900

    2st Week: DB 사용

--- library-app/src/main/java/com/group/libraryapp/domain/user/User.java
+++ library-app/src/main/java/com/group/libraryapp/domain/user/User.java
@@ -6,7 +6,7 @@ public class User {
     private Integer age;
 
     public User(String name, Integer age) {
-        if(name==null||name.isBlank()){
+        if(name == null || name.isBlank()) {
             throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다.", name));
         }
         this.name = name;
